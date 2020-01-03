647ed8f9c4eaa7cb98291028c905d35ef96cd72a
Raina <rcho@elitedigital.ca>
2020-01-03 20:30:13 +0900

    resume changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Portfolio from './components/portfolio/portfolio';
+import ContactUs from './components/contactus/contactus';
+import Footer from './components/footer/footer';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <About />
+      <Portfolio />
+      <ContactUs />
+      <Footer />
     </div>
   );
 }
